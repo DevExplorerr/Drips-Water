a48e14a2e840718e31c01e1756ad973d277f51bd
DevExplorerr <saadj4775@gmail.com>
2025-10-31 19:02:54 +0500

    Drips Water

--- lib/presentation/widgets/forms/custom_text_field.dart
+++ lib/presentation/widgets/forms/custom_text_field.dart
@@ -47,6 +47,7 @@ class CustomTextField extends StatelessWidget {
           obscureText: obscureText ?? false,
           textInputAction: textInputAction,
           autofocus: false,
+          autocorrect: true,
           onFieldSubmitted: onFieldSubmitted,
           decoration: InputDecoration(
             hintText: hintText,
