aeefe622a3ccaaa0234675bad5a893dbf504ca56
DevExplorerr <saadj4775@gmail.com>
2025-11-30 20:09:17 +0500

    refactor: Enhance FavoriteProvider to manage favorite listeners more effectively

--- lib/logic/providers/favorite_provider.dart
+++ lib/logic/providers/favorite_provider.dart
@@ -1,27 +1,47 @@
-import 'package:drips_water/data/services/favorite_service.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:drips_water/data/services/favorite_service.dart';
 
 class FavoriteProvider extends ChangeNotifier {
   final FavoriteService _service;
 
   List<String> favorites = [];
+  StreamSubscription? _favoritesSub;
+  StreamSubscription? _authSub;
 
   FavoriteProvider(this._service) {
-    _listen();
+    _authSub = FirebaseAuth.instance.authStateChanges().listen((user) {
+      _restartListener();
+    });
   }
 
-  void _listen() {
-    _service.listenFavorites().listen((list) {
-      favorites = list;
+  // Restarting listener when user changes
+  void _restartListener() {
+    _favoritesSub?.cancel();
+
+    if (_service.uid == null) {
+      favorites = [];
+      notifyListeners();
+      return;
+    }
+
+    _favoritesSub = _service.listenFavorites().listen((ids) {
+      favorites = ids;
       notifyListeners();
     });
   }
 
-  bool isFavorite(String id) {
-    return favorites.contains(id);
-  }
+  bool isFavorite(String id) => favorites.contains(id);
 
   Future<bool> toggleFavorite(String id) async {
     return _service.toggleFavorite(id, favorites);
+  }
+
+  @override
+  void dispose() {
+    _favoritesSub?.cancel();
+    _authSub?.cancel();
+    super.dispose();
   }
 }
