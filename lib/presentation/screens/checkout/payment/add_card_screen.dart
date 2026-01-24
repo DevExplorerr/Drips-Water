7808d1ff8daa5ebb83a0db249525fb120d8de034
DevExplorerr <saadj4775@gmail.com>
2026-01-24 19:30:58 +0500

    feat: enhance add card screen with edit functionality and improved UI

--- lib/presentation/screens/checkout/payment/add_card_screen.dart
+++ lib/presentation/screens/checkout/payment/add_card_screen.dart
@@ -63,117 +63,135 @@ class _AddCardScreenState extends State<AddCardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Add New Card")),
-      body: SingleChildScrollView(
-        padding: const .all(15),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: .start,
-            children: [
-              Text(
-                "Card Type",
-                style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  fontWeight: .w600,
-                  color: AppColors.secondaryText,
-                ),
-              ),
-              const SizedBox(height: 5),
-              Container(
-                padding: const .symmetric(horizontal: 15),
-                decoration: BoxDecoration(
-                  color: AppColors.grey.withValues(alpha: 0.1),
-                  borderRadius: .circular(8),
-                  border: .all(color: Colors.transparent),
-                ),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    focusColor: AppColors.primary,
-                    value: _selectedCardType,
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down,
-                      color: AppColors.icon,
-                    ),
-                    items: _cardTypes.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(
-                          value,
-                          style: Theme.of(
-                            context,
-                          ).textTheme.bodyMedium?.copyWith(fontWeight: .w500),
-                        ),
-                      );
-                    }).toList(),
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedCardType = newValue!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomTextField(
-                controller: _nameController,
-                labelText: "Card Holder Name",
-                hintText: "e.g. John Doe",
-                textInputType: .name,
-                validator: (v) => v!.isEmpty ? "Required" : null,
-                textInputAction: .next,
-              ),
-              const SizedBox(height: 20),
-              CustomTextField(
-                controller: _numberController,
-                labelText: "Card Number",
-                hintText: "0000 0000 0000 0000",
-                textInputType: .number,
-                validator: (v) => (v!.length < 12) ? "Invalid Number" : null,
-                textInputAction: .next,
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomTextField(
-                      controller: _expiryController,
-                      labelText: "Expiry Date",
-                      hintText: "MM/YY",
-                      textInputType: .datetime,
-                      validator: (v) => v!.isEmpty ? "Required" : null,
-                      textInputAction: .next,
-                    ),
-                  ),
-                  const SizedBox(width: 15),
-                  Expanded(
-                    child: CustomTextField(
-                      controller: _cvvController,
-                      labelText: "CVV",
-                      hintText: "123",
-                      textInputType: .number,
-                      validator: (v) => (v!.length < 3) ? "Invalid CVV" : null,
-                      textInputAction: .done,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.existingCardDetails == null ? "Add New Card" : "Edit Card",
           ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: .only(
-          left: 15,
-          right: 15,
-          top: 15,
-          bottom: MediaQuery.of(context).padding.bottom + 15,
+        body: SingleChildScrollView(
+          padding: const .all(15),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: .start,
+              children: [
+                Text(
+                  "Card Type",
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    fontWeight: .w600,
+                    color: AppColors.secondaryText,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: const .symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: .circular(8),
+                    border: .all(color: AppColors.enabledBorder),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: _selectedCardType,
+                      dropdownColor: AppColors.white,
+                      borderRadius: .circular(12),
+                      icon: const Icon(
+                        Icons.keyboard_arrow_down,
+                        color: AppColors.icon,
+                      ),
+                      items: _cardTypes.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.credit_card,
+                                size: 18,
+                                color: AppColors.primary,
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                value,
+                                style: Theme.of(context).textTheme.bodyMedium
+                                    ?.copyWith(fontWeight: .w500),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _selectedCardType = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                CustomTextField(
+                  controller: _nameController,
+                  labelText: "Card Holder Name",
+                  hintText: "e.g. John Doe",
+                  textInputType: .name,
+                  validator: (v) => v!.isEmpty ? "Required" : null,
+                  textInputAction: .next,
+                ),
+                const SizedBox(height: 20),
+                CustomTextField(
+                  controller: _numberController,
+                  labelText: "Card Number",
+                  hintText: "0000 0000 0000 0000",
+                  textInputType: .number,
+                  validator: (v) => (v!.length < 12) ? "Invalid Number" : null,
+                  textInputAction: .next,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        controller: _expiryController,
+                        labelText: "Expiry Date",
+                        hintText: "MM/YY",
+                        textInputType: .datetime,
+                        validator: (v) => v!.isEmpty ? "Required" : null,
+                        textInputAction: .next,
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: CustomTextField(
+                        controller: _cvvController,
+                        labelText: "CVV",
+                        hintText: "123",
+                        textInputType: .number,
+                        validator: (v) =>
+                            (v!.length < 3) ? "Invalid CVV" : null,
+                        textInputAction: .done,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
-        child: CustomButton(
-          onPressed: _saveCard,
-          height: 50,
-          width: .infinity,
-          text: "Save",
+        bottomNavigationBar: Padding(
+          padding: .only(
+            left: 15,
+            right: 15,
+            top: 15,
+            bottom: MediaQuery.of(context).padding.bottom + 15,
+          ),
+          child: CustomButton(
+            onPressed: _saveCard,
+            height: 50,
+            width: .infinity,
+            text: "Save",
+          ),
         ),
       ),
     );
