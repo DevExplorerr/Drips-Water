7b59b4475991292ffb750ab20ab998460d5850e5
DevExplorerr <saadj4775@gmail.com>
2026-01-28 00:30:57 +0500

    feat: add PromoCodeModel class with mock data for testing

---
+++ lib/data/models/promo_code_model.dart
@@ -0,0 +1,19 @@
+class PromoCodeModel {
+  final String code;
+  final int value;
+  final bool isPercentage;
+
+  PromoCodeModel({
+    required this.code,
+    required this.value,
+    required this.isPercentage,
+  });
+
+  // Mock Data for testing
+  // TODO: (Later fetch from firestore)
+
+  static List<PromoCodeModel> get mockCodes => [
+    PromoCodeModel(code: "WELCOME", value: 10, isPercentage: true),
+    PromoCodeModel(code: "DRIPS20", value: 20, isPercentage: false),
+  ];
+}
