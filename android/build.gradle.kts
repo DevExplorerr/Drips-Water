e01739f561ad3de154e0bc26152a41238618ccc9
DevExplorerr <saadj4775@gmail.com>
2026-08-04 00:42:45 +0500

    Implement structural updates and optimizations across multiple modules

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
     }
 }
 
